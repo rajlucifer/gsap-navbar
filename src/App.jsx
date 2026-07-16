f628f7ac25b5abeb95fb73ab3739bc27c975d4d8
rahul raj <thisisrajtkd18@gmail.com>
2026-07-16 15:12:43 +0530

    firstly initilised all the dependency and tool like gsap @gsap/react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+   
+
+  return (
+     <section className='w-[100vw] h-[100vh] bg-black '>
+      <Navbar/>
+       
+     </section>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Navbar = ()=>{
+    return(
+        <div className="text-white">hello world</div>
+    )
+};
+
+export default Navbar;
