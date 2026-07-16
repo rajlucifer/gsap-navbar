8da89c05f25a870860f3bb0f01a0ae62b6f7c736
rahul raj <thisisrajtkd18@gmail.com>
2026-07-16 19:37:01 +0530

    now scrolling is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,9 @@ function App() {
    
 
   return (
-     <section className='w-[100vw] h-[100vh] bg-black  pt-2'>
+     <section className=' w-full min-h-screen bg-black  pt-2'>
       <Navbar/>
-      <Scroll bgcolor="bg-yellow-600" bgbox="bg-red-600" />
-      <Scroll bgcolor="bg-blue-600" bgbox="bg-gray-500"   />
-      <Scroll bgcolor="bg-green-600" bgbox="bg-orange-600" />
+      <Scroll/>
       
       
        
--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,40 +1,76 @@
-import react from "react"
+import React from "react"
 import { useRef } from "react";
 import gsap from "gsap"
-import {useGSAP} from "@gsap/react"
+import { useGSAP } from "@gsap/react"
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-const Scroll = ({bgcolor,bgbox})=>{
+const Scroll = ({ bgcolor, bgbox }) => {
 
-    const boxRef = useRef(null);
-    const sectionRef = useRef(null);
-   
-   
+    const boxRef1 = useRef(null);
+    const sectionRef1 = useRef(null);
+    const boxRef2 = useRef(null);
+    const sectionRef2 = useRef(null);
+    const boxRef3 = useRef(null);
+    const sectionRef3 = useRef(null);
 
     useGSAP(()=>{
-        gsap.to(boxRef.current,{
+        gsap.to(boxRef1.current,{
             rotate:360,
-            
+            duration:2,
             scrollTrigger:{
-                trigger:sectionRef.current,
+                trigger:sectionRef1.current,
+                start:"50% 50%",
+                end:"80% 20%",
+                scrub:true,
+                markers:true
+            }
+        })
+        gsap.to(boxRef2.current,{
+            rotate:360,
+            duration:2,
+            scrollTrigger:{
+                trigger:sectionRef2.current,
                 start:"top center",
                 end:"bottom top",
+                scrub:true, // is use controll the rotation final and initial by using the scroll 
+                markers:true
+            }
+
+        })
+        gsap.to(boxRef3.current,{
+            rotate:360,
+            duration:2,
+            scrollTrigger:{
+                trigger:sectionRef2.current,
+                start:"40%",
+                end:"bottom top",
                 scrub:true,
-                markers:true,
-                
-            },
-        });
+                markers:true
+            }
 
+        })
 
-    },{scope:sectionRef})
-    
-    return(
-        <section className={`w-[100vw] h-[100vh] ${bgcolor} flex justify-center items-center`}>
-            <div ref={boxRef} className={`w-[200px] h-[200px]  ${bgbox} border-2   border-black rounded-md`}>
-                
-            </div>
-        </section>
+    },[])
+
+    return (
+        <div>
+            <section ref={sectionRef1} className="w-[100vw] h-[100vh] bg-yellow-500 flex justify-center items-center">
+                <div ref={boxRef1} className="w-[200px] h-[200px]  bg-red-500 border-2   border-black rounded-md">
+
+                </div>
+            </section>
+            <section ref={sectionRef2} className="w-[100vw] h-[100vh] bg-green-300 flex justify-center items-center">
+                <div ref={boxRef2} className="w-[200px] h-[200px] bg-emerald-800 border-2   border-black rounded-md">
+
+                </div>
+            </section>
+            <section ref={sectionRef3} className="w-[100vw] h-[100vh] bg-blue-500 flex justify-center items-center">
+                <div ref={boxRef3} className="w-[200px] h-[200px]  bg-cyan-400 border-2   border-black rounded-md">
+
+                </div>
+            </section>
+        </div>
     )
 };
 
